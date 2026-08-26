825d2ba8edbe2fb2f4d76bc3d7a7ee6de6dea309
Suzan Shaikh <suzanshaikh7744@gmail.com>
2026-08-26 20:45:22 +0530

    Delete Core Python/Assignments/Assi2 directory

--- Core Python/Assignments/Assi2/qn12.py
+++
@@ -1,9 +0,0 @@
-num =int(input('Enter a number: '))
-
-d1 = num//100
-d2 = (num//10)%10
-d3 = num %10
-if (d1==d3):
-    print('Number is Palindrome.')
-else:
-    print('Number is not Palindrome.')
--- Core Python/Assignments/Assi2/qn9.py
+++
@@ -1,9 +0,0 @@
-num1 =int(input('Enter number1: '))
-num2 =int(input('Enter number2: '))
-
-num1 = num1+num2
-num2 = num1-num2
-num1 = num1 - num2
-print('after swapping')
-print(num1)
-print(num2)
