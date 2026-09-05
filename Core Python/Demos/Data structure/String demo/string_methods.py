a5425b6bd224eac8b556e5ee34a4ceafdd580b00
Suzan Shaikh <suzanshaikh7744@gmail.com>
2026-09-05 20:09:43 +0530

    Add files via upload

---
+++ Core Python/Demos/Data structure/String demo/string_methods.py
@@ -0,0 +1,26 @@
+str = 'FirstBit Solutions'
+# res = str.capitalize()
+#res = str.count('bit')
+#res = str.endswith('ions')
+#res = str.find('bit')
+# res = str.index('bit')
+# res = str.isalnum()#alpha or numeric
+#res = str.isalpha()
+#res = str.isdigit()
+#res = str.islower()
+#res = str.isspace()
+#res = str.isupper()
+res = ','.join(['101', 'ABC', '70000'])
+#res = str.lower()
+#res = str.replace('Bit', 'Byte')
+#res = str.split('')
+#res = str.startswith('Fir")
+#str = '     [Firstbit solutions)
+###Homework
+#str = "['127.0.0.1', '198.2.1', '135.0.9']"
+##covert to ['127.0.0.1', '198.2.1', '135.0.9']
+#res = str.strip(' []')
+#res = str.swapcase()
+#res = str.title()
+# res = str.upper()
+print(res)
