902723a0855987656969793e4451100663218880
Suzan Shaikh <suzanshaikh7744@gmail.com>
2026-09-05 20:11:03 +0530

    Add files via upload

--- Core Python/Demos/Data structure/List demo/list_methods.py
+++ Core Python/Demos/Data structure/List demo/list_methods.py
@@ -1,5 +1,12 @@
-
-
+li = [20,30,10,40,10]
 # li.append(50)
 # li.clear()
-# li
+# li = li.copy()
+#print(li.count(20))
+# li.extend([60,70,80])
+# print(li.index(50))
+# li.insert(2, 50)
+li.pop(1)
+# li.remove(20)
+# li.reverse
+print(li)
